789ac35bc33e6f7cdd71def325cce8bdd06235e2
Sang Shin <sangshinpassion@gmail.com>
2022-07-08 08:00:56 -0400

    added comment on polymorphism to Department class

--- src/main/java/com/hr/personnel/Department.java
+++ src/main/java/com/hr/personnel/Department.java
@@ -31,6 +31,15 @@ public class Department {
         return numberOfEmployeesWhoWorked;
     }
 
+    // You can observe polymorphic behavior of Java here.
+    // - When "computeMonthlyCompensation()" method is invoked
+    //   on an "Employee" object, what gets invoked is the one
+    //   from the sub-classes.
+    // - In other words, "computeMonthlyCompensation()" method
+    //   of "SalariedEmployee" or "HourlyEmployee" sub-class
+    //   (not the one from the "Employee" class) gets invoked
+    //   depending on what object the "employee" variable
+    //   refers to during runtime
     public double computeDepartmentMonthlyTotalCompensation() {
         double departmentMonthlyTotalCompensation = 0.0;
         for (Employee employee: departmentDAO.getEmployees()) {
